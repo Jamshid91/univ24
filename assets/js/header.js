35a76ab700775dee06fe83649fd045ffabddce9d
Jamshid91 <iymon0217@gmail.com>
2024-02-19 22:37:49 +0500

    added

--- assets/js/header.js
+++ assets/js/header.js
@@ -32,15 +32,56 @@ allLanguageBtn.forEach(btn => {
     })
 })
 
+// const circleAnims = document.querySelectorAll('.circle-anim');
+//       let i = 50;
+//       let a =setInterval(() => {
+//         i= i+2
+//         circleAnims.forEach(anim => {
+//             anim.setAttribute('stroke-dasharray', `${i} 1000`);
+//             if(i == 1000) {
+//                 return i = 50;
+//             }
+//         })
+       
+// }, 25);
+
 const circleAnims = document.querySelectorAll('.circle-anim');
-      let i = 50;
-      let a =setInterval(() => {
-        i= i+2
+console.log(circleAnims)
+
+
+      let i = 2;
+      setInterval(() => {
+        i++
         circleAnims.forEach(anim => {
             anim.setAttribute('stroke-dasharray', `${i} 1000`);
-            if(i == 1000) {
-                return i = 50;
-            }
-        })
-       
-}, 25);
+            if(i == 500) {
+                anim.setAttribute('stroke', '#FF9700');
+                anim.previousElementSibling.setAttribute('stroke', '#0094FF');
+            } 
+            else if (i == 1000) {
+                anim.setAttribute('stroke', '#0094FF');
+                anim.previousElementSibling.setAttribute('stroke', '#FF9700');
+                return i = 1
+            } 
+        });
+}, 1);
+
+// let j = 2;
+// setInterval(() => {
+//     j++
+//     circleAnims3.forEach(anim => {
+//         anim.setAttribute('stroke-dasharray', `${i} 1000`);
+//         if(j == 1000) {
+//             circleAnims4.forEach(anim2 => {
+//                 anim.setAttribute('stroke', '#0094FF');
+//                 anim2.setAttribute('stroke', '#FF9700');
+//             })
+//         } else if (j == 1001) {
+//             circleAnims4.forEach(anim2 => {
+//                 anim.setAttribute('stroke', '#FF9700');
+//                 anim2.setAttribute('stroke', '#0094FF');
+//                 return j = 1
+//             })
+//         }
+//     });
+// }, 1);
